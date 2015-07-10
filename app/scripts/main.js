df14820931389fab9302335e28718dbc3967010d
Todd Heidenreich <jtheidenreich@gmail.com>
2015-07-10 14:23:16 -0400

    landing page stubbed out

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,6 @@
 /* jshint devel:true */
-console.log('\'Allo \'Allo!');
+console.log('Welcome to InTuneTo!');
+
+$("#poo").fitText(0.1, { minFontSize: '20px', maxFontSize: '100px' });
+$("#poo2").fitText(0.5, { minFontSize: '14px', maxFontSize: '60px' });
+$(".increase").fitText(0.5, { minFontSize: '14px', maxFontSize: '48px' });
--- gulpfile.js
+++ gulpfile.js
@@ -79,7 +79,7 @@ gulp.task('clean', require('del').bind(null, ['.tmp', 'dist']));
 gulp.task('serve', ['styles', 'fonts'], function () {
   browserSync({
     notify: false,
-    port: 9000,
+    port: 7000,
     server: {
       baseDir: ['.tmp', 'app'],
       routes: {
