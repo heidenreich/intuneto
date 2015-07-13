6a2f11349761044d82ffc304975f0a8eb6ca9805
Todd Heidenreich <jtheidenreich@gmail.com>
2015-07-13 15:04:36 -0400

    learn more pages and about page, the beginnings

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,11 @@
 /* jshint devel:true */
+'use strict';
+
 console.log('Welcome to InTuneTo!');
 
-$("#poo").fitText(0.1, { minFontSize: '20px', maxFontSize: '100px' });
-$("#poo2").fitText(0.5, { minFontSize: '14px', maxFontSize: '60px' });
-$(".increase").fitText(0.5, { minFontSize: '14px', maxFontSize: '48px' });
+$('#poo').fitText(0.3, { minFontSize: '20px', maxFontSize: '100px' });
+$('#poo2').fitText(0.7, { minFontSize: '14px', maxFontSize: '60px' });
+$('.increase').fitText(0.5, { minFontSize: '14px', maxFontSize: '48px' });
 
 $('.clicker').on('click', function () {
   $('.card').toggleClass('flipped');
