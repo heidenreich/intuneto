e24d51c656b5c0e663e99546d3611864c2bcc358
Todd Heidenreich <jtheidenreich@gmail.com>
2015-07-13 11:44:50 -0400

    stub out about page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,3 +4,8 @@ console.log('Welcome to InTuneTo!');
 $("#poo").fitText(0.1, { minFontSize: '20px', maxFontSize: '100px' });
 $("#poo2").fitText(0.5, { minFontSize: '14px', maxFontSize: '60px' });
 $(".increase").fitText(0.5, { minFontSize: '14px', maxFontSize: '48px' });
+
+$('.clicker').on('click', function () {
+  $('.card').toggleClass('flipped');
+  $('.clicker').toggleClass('hidden');
+});
