0535b49a74fa67f93d21b47b00af3f214c99908d
Todd Heidenreich <jtheidenreich@gmail.com>
2015-07-14 15:59:04 -0400

    styles and more styles

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,11 +3,18 @@
 
 console.log('Welcome to InTuneTo!');
 
-$('#poo').fitText(0.3, { minFontSize: '20px', maxFontSize: '100px' });
-$('#poo2').fitText(0.7, { minFontSize: '14px', maxFontSize: '60px' });
-$('.increase').fitText(0.5, { minFontSize: '14px', maxFontSize: '48px' });
+// using fit text to scale font 
+$('.increase').fitText(0.9, { minFontSize: '14px', maxFontSize: '48px' });
 
+
+// flip the shit on the about page
+// by clicking text or the card itself
 $('.clicker').on('click', function () {
+  $('.card').toggleClass('flipped');
+  $('.clicker').toggleClass('hidden');
+});
+
+$('.card').on('click', function () {
   $('.card').toggleClass('flipped');
   $('.clicker').toggleClass('hidden');
 });
